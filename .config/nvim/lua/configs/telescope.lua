87f05758846b741c78aa9a75a9dc69a11608b272
isMaya404 <franciajeremiah10@gmail.com>
2026-05-15 05:29:39 +0800

    automated commit

--- .config/nvim/lua/configs/telescope.lua
+++ .config/nvim/lua/configs/telescope.lua
@@ -35,7 +35,7 @@ telescope.setup {
     },
     defaults = {
         path_display = { 'filename_first' },
-        file_ignore_patterns = { 'node_modules', 'repomix', '_shared/schemas/' },
+        file_ignore_patterns = { 'node_modules', 'repomix', '_shared/schemas/', '_ignored' },
         mappings = {
             i = {
                 ['<C-t>'] = open_with_trouble,
